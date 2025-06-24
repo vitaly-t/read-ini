fe220e1aea52d6993d8ff0ad32678242e30de077
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2025-06-24 10:06:18 +0100

    adding tests

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -10,4 +10,20 @@ describe('readIniFile', () => {
             VALUE: '123'
         });
     });
+    it('must process sections', () => {
+        const result = readIniFile(`${inputsPath}/complex.ini`, ({key, value}) => {
+            if (key === 'DB_PORT') {
+                return Number(value);
+            }
+            return value;
+        });
+        expect(result).toEqual({
+            NODE_ENV: 'test',
+            DB_HOST: 'localhost',
+            DB_PORT: 123,
+            'my-section': {
+                'extra.value': '123'
+            }
+        });
+    })
 });
